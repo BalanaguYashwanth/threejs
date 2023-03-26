566f5792df2e499cce1a178b2c383ffa9c8f8952
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2023-03-26 15:36:40 +0530

    Update index.js

--- Rendering3DObjects/src/index.js
+++ Rendering3DObjects/src/index.js
@@ -1,75 +1,62 @@
 import * as THREE from "three";
-// import * as THREE from "./three.js-master/build/three.module.js";
-// import { GLTFLoader } from './three.js-master/examples/jsm/loaders/GLTFLoader.js'
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
-import sonyLogo from '../Sony_logo.svg'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import {
+  CSS2DObject,
+  CSS2DRenderer,
+} from "three/examples/jsm/renderers/CSS2DRenderer";
+import sonyLogo from "../Sony_logo.svg";
 
-
-const canvas = document.querySelector(".webgl");
+// const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
 
-const modelURL = new URL('../exhibition_stand.glb', import.meta.url)
-const assetLoader  = new GLTFLoader();
-assetLoader.load( modelURL.href,function(glb){
-  console.log(glb,'hello',modelURL.href);
-  const model = glb.scene;
-  model.scale.set(0.09,0.09,0.09)
-  scene.add(model);
-  // model.position.set(0,2,0);
-}, 
+const p = document.createElement("p");
+p.textContent = "Helloooooooooo";
+const cPointLabel = new CSS2DObject(p);
+scene.add(cPointLabel);
+cPointLabel.position.set(1, 1, 1);
+
+let model;
+const modelURL = new URL("../exhibition_stand.glb", import.meta.url);
+const assetLoader = new GLTFLoader();
+assetLoader.load(
+  modelURL.href,
+  function (glb) {
+    console.log(glb, "hello", modelURL.href);
+    model = glb.scene;
+    model.scale.set(0.09, 0.09, 0.09);
+    scene.add(model);
+
+    // model.position.set(0,2,0);
+  },
   function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + " ====  100% loaded");
-    }, function(error){
-  console.error(error);
-});
+    console.log((xhr.loaded / xhr.total) * 100 + " ====  100% loaded");
+  },
+  function (error) {
+    console.error(error);
+  }
+);
 
-const light = new THREE.DirectionalLight(0xfffff,1)
-light.position.set(2,2,5)
-scene.add(light)
-
-//box and plane
-// const textureLoader = new THREE.TextureLoader();
-// const geometry = new THREE.PlaneGeometry(-1, -1);
-// const material = new THREE.MeshBasicMaterial({
-//   color: "lightgreen",
-//   map: textureLoader.load(sonyLogo)
-// });
-// const boxMesh = new THREE.Mesh(geometry, material);
-// scene.add(boxMesh);
-
-
-// const planeGeometry = new THREE.PlaneGeometry(5,5);
-// const planeMaterial = new THREE.MeshBasicMaterial({color:0x00FF00});
-// const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-// scene.add(plane);
-// plane.position(0,0,10);
-
-
-// const texture = new THREE.TextureLoader().load(sonyLogo);
-// const material = new THREE.MeshBasicMaterial( { map: texture } );
-// scene.add(material)
+const light = new THREE.DirectionalLight(0xfffff, 1);
+light.position.set(2, 2, 5);
+scene.add(light);
 
 //Boiler Plate code
 const sizes = {
-  width: 500,
-  height: 500,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 const camera = new THREE.PerspectiveCamera(
   75,
-  sizes.width /  sizes.height,
+  sizes.width / sizes.height,
   0.1,
   100
 );
 camera.position.set(0, 1, 2);
-scene.add(camera);
-
-
-
 
 const renderer = new THREE.WebGL1Renderer();
-// renderer.setClearColor('blue') //setting background color
+renderer.setClearColor("white"); //setting background color
 renderer.setSize(sizes.width, sizes.height);
 
 document.body.appendChild(renderer.domElement);
@@ -77,16 +64,34 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.shadowMap.enabled = true;
 renderer.gammaOutput = true;
 
-const orbit  = new OrbitControls(camera, renderer.domElement);
-
+const orbit = new OrbitControls(camera, renderer.domElement);
 const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper)
+scene.add(axesHelper);
 orbit.update();
-// renderer.render(scene, camera);
 
-function animate(){
-  requestAnimationFrame(animate)
-  renderer.render(scene,camera)
-}
+const followText = document.getElementById("follow-text");
+const canvas = document.querySelector("canvas");
+const boxPosition = new THREE.Vector3();
+let boxPositionOffset = new THREE.Vector3();
 
-animate()
+//GAMELOOP
+
+const clock = new THREE.Clock();
+let gameLoop = () => {
+  // console.log('model--->', model)
+  // MOVE TEXT
+  if (model) {
+    boxPosition.setFromMatrixPosition(model.matrixWorld);
+    boxPosition.add(boxPositionOffset);
+    boxPosition.project(camera);
+
+    var widthHalf = window.innerWidth / 2,heightHalf = window.innerHeight / 2;
+    boxPosition.x = (boxPosition.x * widthHalf) + widthHalf;
+    boxPosition.y = - (boxPosition.y * heightHalf) + heightHalf;
+    followText.style.top = `${boxPosition.y}px`;
+    followText.style.left = `${boxPosition.x}px`;
+  }
+  renderer.render(scene, camera);
+  requestAnimationFrame(gameLoop);
+};
+gameLoop();
